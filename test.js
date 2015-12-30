47617928a6eb36b8e9afe2e4f0a10cba4b470a08
wemee <wemee7012@gmail.com>
2015-12-30 14:47:39 +0800

    使用真實資料填寫分房間設備

---
+++ test.js
@@ -0,0 +1,81 @@
+// rooms = [{room:"2樓教室外", plugs: [{deivce:"飲水機 26", devId: "20F85EAA115D", volt: "220"},
+//                            {deivce:"飲水機 25", devId: "20F85EAA143D", volt: "220"}]
+//  }, {room:"總務處",	plugs: [{deivce:"總務處印表機",	devId:"20F85EAA7575",	volt: "100"}]
+//  }, {room:"會議室", plugs: [{device:"會議室設備 _1",	devId: "20F85EA963F8",	volt: "100"},
+//                           {device:"會議室設備 _2",	devId: "20F85EA972D9",	volt: "100"},
+//                           {device:"會議室設備 _3",	devId: "20F85EA96E72",	volt: "100"},
+//                           {device:"會議室設備 _4",	devId: "20F85EAA7654",	volt: "100"},
+//                           {device:"會議室設備 _5",	devId: "20F85EA96E6E",	volt: "100"},
+//                           {device:"會議室 燈 前",	devId: "20F85EA972D1",	volt: "100"},
+//                           {device:"會議室 燈 後",	devId: "20F85EA96E8C",	volt: "100"},
+//                           {device:"會議室 風扇 前",	devId: "20F85EA966C3",	volt: "100"},
+//                           {device:"會議室 風扇 後",	devId: "20F85EAA1508",	volt: "100"}]
+//  }, {room:"智慧教室", plugs: [{device: "燈", devId: "20F85EAA29E3", vlot: "100"},
+//                            {device: "風扇", devId: "20F85EAA0D75", vlot: "100"},
+//                            {device: "電子白版",devId: "20F85EA20F85EA", vlot: "100"}]
+//  },{room: "105 教室", plugs: [{device: "燈", devId: "20F85EAA74A6", volt: "100"},
+//                            {device: "風扇", devId: "20F85EAA508B", volt: "100"},
+//                            {device: "平板充電車", devId: "20F85EAA0F47", volt: "100"},
+//                            {device: "電子白版", devId: "20F85EAA6D9B", volt: "100"}]
+//  }, {room: "106 教室", plugs: [{device: "燈", devId: "20F85EAA14DB", volt: "100"},
+//                                {device: "風扇", devId: "20F85EAA104A", volt: "100"},
+//                                {device: "平板充電車", devId: "20F85EAA764C", volt: "100"},
+//                                {device: "電子白版", devId: "20F85EAA7746", volt: "100"}]
+//  }, {room: "205 教室", plugs: [{device: "燈", devId: "20F85EAA14C9", volt: "100"},
+//                                {device: "風扇", devId: "20F85EAA10EF", volt: "100"},
+//                                {device: "平板充電車", devId: "20F85EAA753A", volt: "100"},
+//                                {device: "電子白版", devId: "20F85EAA51B7", volt: "100"}]
+//  }, {room: "206 教室", plugs: [{device: "燈", devId: "20F85EA95CC2", volt: "100"},
+//                                {device: "風扇", devId: "20F85EA95A6A", volt: "100"},
+//                                {device: "平板充電車", devId: "20F85EAA54DD", volt: "100"},
+//                                {device: "電子白版", devId: "20F85EAA73BB", volt: "100"}]
+//  }, {room: "207 教室", plugs: [{device: "燈", devId: "20F85EAA2A35", volt: "100"},
+//                                {device: "風扇", devId: "20F85EAA100E", volt: "100"},
+//                                {device: "平板充電車", devId: "20F85EA95D19", volt: "100"},
+//                                {device: "電子白版", devId: "20F85EAA766F", volt: "100"}]
+//  }, {room: "302 教室", plugs: [{device: "燈", devId: "20F85EA96732", volt: "100"},
+//                                {device: "風扇", devId: "20F85EA95C63", volt: "100"},
+//                                {device: "電子白版", devId: "20F85EAA7515", volt: "100"}]
+//  }];
+
+
+ rooms = {"a": [{deivce:"飲水機 26", devId: "20F85EAA115D", volt: "220"},
+                {deivce:"飲水機 25", devId: "20F85EAA143D", volt: "220"}],
+          "b": [{deivce:"總務處印表機",	devId:"20F85EAA7575",	volt: "100"}],
+          "c": [{device:"會議室設備 _1",	devId: "20F85EA963F8",	volt: "100"},
+                {device:"會議室設備 _2",	devId: "20F85EA972D9",	volt: "100"},
+                {device:"會議室設備 _3",	devId: "20F85EA96E72",	volt: "100"},
+                {device:"會議室設備 _4",	devId: "20F85EAA7654",	volt: "100"},
+                {device:"會議室設備 _5",	devId: "20F85EA96E6E",	volt: "100"},
+                {device:"會議室 燈 前",	devId: "20F85EA972D1",	volt: "100"},
+                {device:"會議室 燈 後",	devId: "20F85EA96E8C",	volt: "100"},
+                {device:"會議室 風扇 前",	devId: "20F85EA966C3",	volt: "100"},
+                {device:"會議室 風扇 後",	devId: "20F85EAA1508",	volt: "100"}],
+          "d": [{device: "燈", devId: "20F85EAA29E3", vlot: "100"},
+                {device: "風扇", devId: "20F85EAA0D75", vlot: "100"},
+                {device: "電子白版",devId: "20F85EA20F85EA", vlot: "100"}],
+          "r105": [{device: "燈", devId: "20F85EAA74A6", volt: "100"},
+                  {device: "風扇", devId: "20F85EAA508B", volt: "100"},
+                  {device: "平板充電車", devId: "20F85EAA0F47", volt: "100"},
+                  {device: "電子白版", devId: "20F85EAA6D9B", volt: "100"}],
+          "r106": [{device: "燈", devId: "20F85EAA14DB", volt: "100"},
+                  {device: "風扇", devId: "20F85EAA104A", volt: "100"},
+                  {device: "平板充電車", devId: "20F85EAA764C", volt: "100"},
+                  {device: "電子白版", devId: "20F85EAA7746", volt: "100"}],
+          "r205": [{device: "燈", devId: "20F85EAA14C9", volt: "100"},
+                  {device: "風扇", devId: "20F85EAA10EF", volt: "100"},
+                  {device: "平板充電車", devId: "20F85EAA753A", volt: "100"},
+                  {device: "電子白版", devId: "20F85EAA51B7", volt: "100"}],
+          "r206": [{device: "燈", devId: "20F85EA95CC2", volt: "100"},
+                  {device: "風扇", devId: "20F85EA95A6A", volt: "100"},
+                  {device: "平板充電車", devId: "20F85EAA54DD", volt: "100"},
+                  {device: "電子白版", devId: "20F85EAA73BB", volt: "100"}],
+          "r207": [{device: "燈", devId: "20F85EAA2A35", volt: "100"},
+                  {device: "風扇", devId: "20F85EAA100E", volt: "100"},
+                  {device: "平板充電車", devId: "20F85EA95D19", volt: "100"},
+                  {device: "電子白版", devId: "20F85EAA766F", volt: "100"}],
+          "r302": [{device: "燈", devId: "20F85EA96732", volt: "100"},
+                  {device: "風扇", devId: "20F85EA95C63", volt: "100"},
+                  {device: "電子白版", devId: "20F85EAA7515", volt: "100"}]};
+
+console.log(rooms);
